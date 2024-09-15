215f8aad8cf6f5afa398704b5630ee3efadd01d4
maxming2333 <maxming2333@gmail.com>
2024-09-16 02:18:36 +0800

    feat: 提交完善代码

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # mweb-to-obsidian
-mweb 转换为 obsidian
+
+将 mweb 迁移为 obsidian
+
+----------
+
+## 功能特性：
+- 自动读取文件夹数据库，生成对应的文件夹层级
+- 自动读取文件夹/文件顺序，基于 [obsidian-bartender](https://github.com/nothingislost/obsidian-bartender) 配置文件
+- 自动导出媒体内容
+- 基于用户配置
+  - 自动根据文档内容的 H1 标题 `重命名` 文件名
+  - 自动去除文档顶部的 H1 标题
+
+### 插件配合
+
+安装以下 obsidian 插件配合本脚本，食用更佳：
+- [Bartender](https://github.com/nothingislost/obsidian-bartender)，此插件能保证文件顺序
+  - 此插件需要先安装 [BRAT](https://github.com/TfTHacker/obsidian42-brat)，然后再从 BRAT 中安装插件
+- [Front Matter Title](https://github.com/snezhig/obsidian-front-matter-title)，此插件能保证在文件列表中自动显示文件内容的 H1 为文件名
+  - 安装了这个插件，脚本里面的 `renameFile` 和 `removeTitle` 就都可以设置为 `false` 了
+- [Hidden Folder](https://github.com/ptrsvltns/hidden-folder-obsidian)
+  - 这个可以把 `media` 文件夹隐藏
+
+## 如何使用
+
+必要环境：[Node.js](https://nodejs.org)
+
+### 安装项目依赖
+进入到此项目根目录下，运行：
+
+```bash
+npm install
+```
+
+### 修改项目配置
+在 [index.js](index.js) 中找到 `用户配置区域` 进行对应的 `路径` 和 `开关` 配置即可
+
+```js
+// 指定 Mweb 文档库地址
+const mwebPath = path.join(os.homedir(), 'Library/Containers/com.coderforart.MWeb3/Data/Library/Application Support/MWebLibrary');
+
+// 指定 obsidian 文档库路径，我这里放在我项目下面，避免出现自动覆盖写入，导致之前的文档库资料覆盖
+const ObsidianPath = path.join(__dirname, 'backup');
+
+// 指定 obsidian-bartender 插件地址
+const bartenderPluginData = path.join(ObsidianPath, '.obsidian/plugins/obsidian-bartender/data.json');
+
+// 是否重命名文件，如果配置为 true，将会读取文件的 h1 作为文件名
+const renameFile = false;
+// 是否去除标题，如果配置为 true，那么就会自动去掉文档库的 H1 标题
+const removeTitle = false;
+```
+
+### 运行
+
+使用以下命令，就可以在配置的目录下，看到对应的文件生成：
+```bash
+node ./index.js
+```
+
--- index.js
+++ index.js
@@ -1,51 +1,149 @@
-const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const fs = require('fs-extra');
 const { mkdirp } = require('mkdirp');
+const { glob } = require('glob');
+const filenamify = require('filenamify');
+const _ = require('lodash');
 
+/* ==================== 用户配置区域 ==================== */
+// 指定 Mweb 文档库地址
 const mwebPath = path.join(os.homedir(), 'Library/Containers/com.coderforart.MWeb3/Data/Library/Application Support/MWebLibrary');
+
+// 指定 obsidian 文档库路径，我这里放在我项目下面，避免出现自动覆盖写入，导致之前的文档库资料覆盖
+const ObsidianPath = path.join(__dirname, 'backup');
+
+// 指定 obsidian-bartender 插件地址
+const bartenderPluginData = path.join(ObsidianPath, '.obsidian/plugins/obsidian-bartender/data.json');
+
+// 是否重命名文件，如果配置为 true，将会读取文件的 h1 作为文件名
+const renameFile = false;
+// 是否去除标题，如果配置为 true，那么就会自动去掉文档库的 H1 标题
+const removeTitle = false;
+
+/* ==================== 自动配置区域 ==================== */
+const docsPath = path.join(mwebPath, 'docs');
 const dbFile = path.join(mwebPath, 'mainlib.db');
+
+
 const knex = require('knex')({
   client: 'sqlite3',
   connection: {
     filename: dbFile,
   },
+  useNullAsDefault: true,
 });
 
-async function makeDir(id, dir, dirMap = {}) {
+
+const infoMap = {
+  dir: {},
+  order: {},
+};
+
+function getName(name) {
+  return filenamify(name, { replacement: '_' });
+}
+
+async function makeInfo(id, dir) {
   const dirList = await knex.raw(`SELECT * FROM cat WHERE pid = ${id}`);
   for (let row of dirList) {
-    const dirName = path.join(dir, row.name);
-    const newDir = path.join(__dirname, 'backup', dirName);
-    dirMap[row.uuid] = newDir;
-    console.log('创建文件夹：', newDir);
-    mkdirp.sync(newDir);
-    await makeDir(row.uuid, dirName, dirMap);
+    const dirName = path.join(dir, getName(row.name));
+    const fileDir = path.join(ObsidianPath, dirName);
+    infoMap.dir[row.uuid] = fileDir;
+    infoMap.order[dir] = infoMap.order[dir] || [];
+    infoMap.order[dir][row.sort] = dirName;
+    await makeInfo(row.uuid, dirName);
   }
-  return dirMap;
+  if (infoMap.order[dir]) {
+    infoMap.order[dir] = infoMap.order[dir].filter(n => n);
+  }
+}
+
+function copyFile(fileId, baseDir) {
+  const sourceFile = path.join(docsPath, `${fileId}.md`);
+  if (!fs.existsSync(sourceFile)) return;
+  const fileInfo = convertFile(sourceFile);
+  const name = renameFile ? fileInfo.newName : fileInfo.name;
+  const content = removeTitle ? fileInfo.newContent : fileInfo.content;
+  const newFilePath = path.join(baseDir, `${name}.md`);
+  mkdirp.sync(baseDir);
+  fs.writeFileSync(newFilePath, content);
+  console.log('[COPY]:', sourceFile, '-->', newFilePath);
+  return {
+    from: sourceFile,
+    to: newFilePath
+  };
+}
+
+function copyMedia() {
+  const sourceMedia = path.join(docsPath, 'media');
+  const newMedia = path.join(ObsidianPath, 'media');
+  mkdirp.sync(newMedia);
+  fs.copySync(sourceMedia, newMedia);
+}
+
+function convertFile(file) {
+  const baseName = path.basename(file, '.md');
+  const text = fs.readFileSync(file).toString().trim();
+  // 去除一些空的 #
+  const content = text.replace(/^\s*#+\s*/ig, '').trim();
+  const result = /^(.*)\s*([\s\S]*)/ig.exec(content);
+  const fileName = !result || result.length < 2 ? baseName : getName(result[1]);
+  return {
+    name: baseName,
+    newName: fileName.replace(/^#*(.*)$/ig, '$1').trim(),
+    content: text,
+    newContent: result[2],
+  }
+}
+
+function setOrder(orderData) {
+  let data = {};
+  try {
+    if (fs.existsSync(bartenderPluginData)) {
+      const text = fs.readFileSync(bartenderPluginData).toString().trim();
+      data = JSON.parse(text);
+    }
+  } catch (error) {}
+  const newData = _.mergeWith(data, { fileExplorerOrder: orderData }, function (objValue, srcValue) {
+    if (_.isArray(objValue)) {
+      return objValue.concat(srcValue);
+    }
+  });
+  mkdirp.sync(path.dirname(bartenderPluginData));
+  fs.writeFileSync(bartenderPluginData, JSON.stringify(newData, null, 2));
+  console.log('[SET_ORDER]:', bartenderPluginData);
 }
 
 async function main() {
-  const dirMap = await makeDir(0, '');
-  const dirIdList = Object.keys(dirMap);
+  const docFiles = await glob(`${docsPath}/**.md`);
+  const mwebFiles = new Set(docFiles);
+
+  await makeInfo(0, '');
+
+  setOrder(infoMap.order);
+
+  const dirIdList = Object.keys(infoMap.dir);
   for (let rid of dirIdList) {
     const articleList = await knex.raw(`SELECT * FROM cat_article WHERE rid = ${rid}`);
     for (let article of articleList) {
-      const mdFile = path.join(mwebPath, 'docs', `${article.aid}.md`);
-      if (fs.existsSync(mdFile)) {
-        const text = fs.readFileSync(mdFile).toString().trim();
-        let fileName = text.replace(/^\s*#+\s*/ig, '').trim().match(/^\s*(\S+).*/ig);
-        if (!fileName) continue;
-        if (/\/|\\|\:/i.test(fileName)) {
-          console.log(fileName);
-          fileName = article.a
-        }
-        // const newFilePath = path.join(dirMap[rid], `${fileName}.md`);
-        // console.log('拷贝 Markdown：', mdFile, '-->', newFilePath);
-        // fs.copyFileSync(mdFile, newFilePath);
-      }
+      const { from } = copyFile(article.aid, infoMap.dir[rid]);
+      // 去除有数据映射关系的文件
+      mwebFiles.delete(from);
     }
   }
+
+  // 如果发现还有一些文件，没有数据库映射，那么就放入 “未分类”
+  for (let doc of mwebFiles) {
+    const fileId = path.basename(doc, '.md');
+    const newDir = path.join(ObsidianPath, '未分类');
+    copyFile(fileId, newDir);
+  }
+
+  copyMedia();
+
+  knex.destroy();
 }
 
 main();
+
